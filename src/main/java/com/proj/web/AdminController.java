cf1bdb2f7954ae4f12353f87c08b8c5568c86b39
Touri Mohamed Amine <touri.med.amine@gmail.com>
2021-01-25 01:55:01 +0100

    Ajouter Professeur

--- src/main/java/com/proj/web/AdminController.java
+++ src/main/java/com/proj/web/AdminController.java
@@ -1,18 +1,25 @@
 package com.proj.web;
 
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import com.proj.dao.CompteRepository;
 import com.proj.dao.Niv_FilRepository;
+import com.proj.dao.ProfesseurRepository;
+import com.proj.entities.Compte;
+import com.proj.entities.Element;
 import com.proj.entities.Niv_Fil;
+import com.proj.entities.Professeur;
 
 
 
@@ -22,6 +29,12 @@ public class AdminController {
 	
 	@Autowired
 	private Niv_FilRepository niv_filRepository;
+	
+	@Autowired
+	private CompteRepository compteRepository;
+	
+	@Autowired
+	private ProfesseurRepository professeurRepository;
 	
 	
 	@RequestMapping(value="/formNivFil", method=RequestMethod.GET)
@@ -39,6 +52,29 @@ public class AdminController {
 		
 		niv_filRepository.save(niv_fil);
 		
+		return "redirect:/admin";
+	}
+	
+	@RequestMapping(value="/formProfesseur", method=RequestMethod.GET)
+	public String formProfesseur(Model model) {
+		model.addAttribute("compte", new Compte());
+		return "formProfesseur";
+	}
+	
+	@RequestMapping(value="/saveProfesseur", method=RequestMethod.POST)
+	public String saveProfesseur(Model model, @Valid Compte compte, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			return "formProfesseur";
+		}
+		compte.setRole("PROFESSEUR");
+		compte.setActive(true);
+		compteRepository.save(compte);
+		
+		Professeur professeur = new Professeur();
+		professeur.setCompte(compte);
+		professeurRepository.save(professeur);
+
 		return "redirect:/admin";
 	}
 
